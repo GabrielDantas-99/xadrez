d7276593f18fb76dbf83543e509fac03fdf36f4d
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-04-11 20:27:25 -0300

    Movendo pecas

--- jogo-xadrez/src/application/Program.java
+++ jogo-xadrez/src/application/Program.java
@@ -1,14 +1,34 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		// Instanciando uma partida
 		ChessMatch chessMatch = new ChessMatch();
-		// Imprimindo as peças da partida
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true) {
+			// Imprimindo as peças da partida
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		
+		
 		
 	}
 
